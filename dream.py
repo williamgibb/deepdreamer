0f5d754c023c5367ee7e15776a305c6de3e1d009
William Gibb <williamgibb@gmail.com>
2015-09-10 21:04:26 -0400

    Serialize the config after processing a file. Add a march function which will iterate over a range of configuration values.

--- dream.py
+++ dream.py
@@ -30,21 +30,31 @@ def make_output_fp(config):
     bn, extension = input_fn.rsplit('.', 1)
     now = datetime.datetime.utcnow()
     timestamp = now.strftime('%Y%m%d_%H%M%S')
-    output_fn = '{bn}_{ts}.{ext}'.format(bn=bn,
-                                         ts=timestamp,
-                                         ext=extension)
+    fn_fmt = '{bn}_{ts}.{ext}'
+    output_fn = fn_fmt.format(bn=bn,
+                              ts=timestamp,
+                              ext=extension)
+    config_output_fn = fn_fmt.format(bn=bn,
+                                     ts=timestamp,
+                                     ext='.json')
     output_fp = os.path.join(config.get('output'), output_fn)
-    return output_fp
+    config_output_fp = os.path.join(config.get('output'), config_output_fn)
+    return output_fp, config_output_fp
 
 
-def run_dream(config):
-    output_fp = make_output_fp(config)
-    dreamp = proc.Proc(model_path=config.get('model_path'),
-                       model_name=config.get('model_name'))
+def run_dream(config, dreamp=None):
+    output_fp, config_output_fp = make_output_fp(config)
+    if not dreamp:
+        dreamp = proc.Proc(model_path=config.get('model_path'),
+                           model_name=config.get('model_name'))
 
     dreamp.process_image(input_fp=config.get('input'),
                          output_fp=output_fp,
                          **config.get('deepdream_params'))
+
+    with open(config_output_fp, 'wb') as f:
+        f.write(json.dumps(config))
+
     return True
 
 
@@ -56,10 +66,15 @@ def dream_output_layers(config):
     for layer in layers:
         log.info('Running layer [{}]'.format(layer))
         config['deepdream_params']['end'] = layer
-        output_fp = make_output_fp(config)
-        dreamp.process_image(input_fp=config.get('input'),
-                             output_fp=output_fp,
-                             **config.get('deepdream_params'))
+        run_dream(config, dreamp=dreamp)
+
+
+def march(config, param, start, end, increment):
+    dreamp = proc.Proc(model_path=config.get('model_path'),
+                       model_name=config.get('model_name'))
+    for i in range(start, end, increment):
+        config['deepdream_params'][param] = i
+        run_dream(config, dreamp=dreamp)
 
 
 def dump_layers(config):
